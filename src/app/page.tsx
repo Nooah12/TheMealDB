e2cf5a3d5c9cb00d728dbeb62f5d6fae987054d0
Noah Gordon <noah.o.gordon@gmail.com>
2025-04-03 15:57:22 +0200

    added baloo font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 
 import {Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter} from "@/components/ui/card"
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const { user } = useUserContext() as UserContextType // treat this as a specific type! not null?
@@ -40,25 +41,30 @@ export default function Home() {
             <p className="font-semibold text-lg mb-1">Welcome back {user.name}!</p>
             <p>Your favorite category of food is: <span className="font-bold">{user.category}</span></p>
           </div>
-          <div className="grid md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-6 px-8">
+          <div className="grid md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-6 px-8 max-w-screen-2xl m-auto">
             {user.category.length > 0 ? (
               recipes && recipes.map((meal: RecipeType) => (
                 // Card
 
 
-                <Card key={meal.idMeal} className="flex flex-col justify-between bg-gray-100 p-4 border border-gray-300 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+                <Card key={meal.idMeal} className="flex flex-col justify-between bg-gray-100 border border-gray-300 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
                   <CardHeader>
                     <CardTitle>{meal.strMeal}</CardTitle>
                   </CardHeader>
                   <CardContent>
                     <Link href={`/recipe/${meal.idMeal}`}>
                       <img 
-                        className="w-full h-auto object-cover rounded-lg mb-2 border border-[#7f7575]" 
+                        className="w-full h-auto object-cover rounded-lg border border-[#7f7575]" 
                         src={meal.strMealThumb} 
                         alt={meal.strMeal} 
                       />
                     </Link>
                   </CardContent>
+                  <CardFooter>
+                    <Link href={`/recipe/${meal.idMeal}`}>
+                      <Button variant="outline" className="bg-orange-300">View Recipe</Button>
+                    </Link>
+                  </CardFooter>
                 </Card>
 
               ))
